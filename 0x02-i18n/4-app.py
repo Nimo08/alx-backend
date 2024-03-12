ab4db7546ad6a3fb8fc49f5193fa00179935c390
Nimo08 <nimomohamed800@gmail.com>
2024-03-12 12:57:58 +0300

    Task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Force locale with URL parameter"""
+
+from flask import Flask, request, render_template
+from flask_babel import Babel, _
+
+
+class Config:
+    """Flask app config"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """Return 4-index.html"""
+    return render_template("4-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Determing preferred locale"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# babel.init_app(app, locale_selector=get_locale)
+
+
+if __name__ == "__main__":
+    app.run()
