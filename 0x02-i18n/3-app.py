82c597d554e1356d9a818422078cef6d67e59cc0
Nimo08 <nimomohamed800@gmail.com>
2024-03-12 12:42:50 +0300

    Use request in get_locale

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -27,8 +27,11 @@ def index() -> str:
 @babel.localeselector
 def get_locale() -> str:
     """Determing preferred locale"""
-    return 'en'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
+# babel.init_app(app, locale_selector=get_locale)
+# Use the above instead of @babel.localeselector to avoid error
 
 if __name__ == "__main__":
     app.run(debug=True)
